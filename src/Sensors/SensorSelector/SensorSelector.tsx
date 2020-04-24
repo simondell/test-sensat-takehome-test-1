400ec295c43e5cc61f0fe07319e009476bb81fd7
Simon Dell <simon.dell@founders4schools.org.uk>
2020-04-24 15:05:04 +0100

    Begin integrating google-maps-react

--- src/Sensors/SensorSelector/SensorSelector.tsx
+++ src/Sensors/SensorSelector/SensorSelector.tsx
@@ -1,6 +1,32 @@
 import React from 'react'
+import {
+  GoogleApiWrapper,
+  InfoWindow,
+  Map,
+  Marker,
+} from 'google-maps-react'
+import Spinner from '../../Shared/Spinner/Spinner'
 
-export default () =>
-  <section>
-    <h2>Sensor Selector</h2>
-  </section>
+interface SensorSelectorProps {
+  google: typeof google
+}
+
+function SensorSelector (props: SensorSelectorProps) {
+  return (
+    <section>
+      <h2>Sensor Selector</h2>
+      <div
+        id="map-wrapper"
+      >
+        <Map google={props.google} />
+      </div>
+    </section>
+  )
+}
+
+const mapConfig = {
+  apiKey: process.env.REACT_APP_GOOGLE_API_KEY || '',
+  LoadingContainer: Spinner,
+}
+
+export default GoogleApiWrapper(mapConfig)(SensorSelector)
